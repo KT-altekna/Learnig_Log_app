46b191eeaa6a7ccdfe1910bc1965342ab5dd9f84
KT-altekna <kuroki.togo@gmail.com>
2026-01-15 10:20:08 +0900

    Add files via upload
    実行ファイル, envファイル, requirements.txt の追加

---
+++ run.py
@@ -0,0 +1,45 @@
+from apps import create_app
+from apps.extentions import db,login_manager
+from apps.blueprints.auth.routes import auth_bp
+from apps.blueprints.user.routes import user_bp
+from apps.blueprints.plans.routes import plans_bp
+from apps.blueprints.records.routes import records_bp
+from apps.blueprints.public.routes import public_bp
+from apps.blueprints.favorite.routes import favorite_bp
+from apps.models.user import Users
+from flask_migrate import Migrate
+
+# インスタンス生成
+app = create_app()
+
+# ユーザー識別用の関数
+@login_manager.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
+
+# blueprintを登録
+app.register_blueprint(auth_bp)
+app.register_blueprint(user_bp)
+app.register_blueprint(plans_bp)
+app.register_blueprint(records_bp)
+app.register_blueprint(public_bp)
+app.register_blueprint(favorite_bp)
+
+# DB接続の設定
+"""
+DB_INFO = {
+    'user' : 'appuser',
+    'password':'Flaskpassword',
+    'host':'localhost',
+    'name':'learning_log'
+}
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{name}'.format(**DB_INFO)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+"""
+
+# Migrateとアプリ連携
+Migrate(app, db)
+
+# アプリの起動
+if __name__=="__main__":
+    app.run(debug=True)
