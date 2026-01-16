397253c125baaf6d12f3d5682f20a238a4abdfdc
KT-altekna <kuroki.togo@gmail.com>
2026-01-16 15:28:35 +0900

    Update config.py
    🔹 Render の古い URL 互換対策
    🔹 SQLAlchemy でのエラー防止

--- apps/config.py
+++ apps/config.py
@@ -9,3 +9,7 @@ class DevelopmentConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+            "postgres://", "postgresql://", 1
+        )
